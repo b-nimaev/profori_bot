541dadd4c98cd6cb7b140978f841d56fcedd152c
b-nimaev <moyhoroshenkiy@gmail.com>
2023-05-29 08:58:13 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import 'dotenv/config';
 import { Telegraf, Scenes, session, Composer } from 'telegraf';
-import set_webhook from './bot/utils/webhook';
 import proforiContext from './bot/models/ProforiContext';
 import { ExtraEditMessageText } from 'telegraf/typings/telegram-types';
 
@@ -122,5 +121,25 @@ const stage: any = new Scenes.Stage<proforiContext>([home], { default: 'home' })
 
 bot.use(session())
 bot.use(stage.middleware())
+
+async function set_webhook() {
+    if (process.env.MODE === 'production') {
+        bot.telegram.setWebhook(
+            `https://profori.pro/bot`
+        ).then(() => {
+            console.log('webhook setted')
+        });
+    } else {
+        await fetch('http://localhost:4040/api/tunnels')
+            .then((res: { json: () => any; }) => res.json())
+            .then((json: { tunnels: any[]; }) => json.tunnels.find(tunnel => tunnel.proto === 'https'))
+            .then((secureTunnel: { public_url: any; }) => bot.telegram.setWebhook(`${secureTunnel.public_url}/bot`))
+            .then(async (status: any) => {
+                console.log(`webhook setted: ${status}`)
+            })
+    }
+}
+
+set_webhook()
 
 export { bot }
